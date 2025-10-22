7bef460c51731e573b30cea7d2c487f9ce08f3a3
Victor M. Nascimento <72674683+vicmoyses2@users.noreply.github.com>
2025-10-22 10:30:01 -0300

    Atualização automática via Cursor

---
+++ FoundryFundamentals/contracts/text.sol
@@ -0,0 +1,7 @@
+// SPDX-Licence-Identifier: MIT
+pragma ^0.8.24;
+//teste
+// teste asds asd as sasa 
+contract Text {
+    
+}
--- FoundryFundamentals/text.sol
+++
@@ -1,5 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.24;
-//teste
-// teste asds asd as sasa 
-contract Text {}
